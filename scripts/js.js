60f3337345f6c84081abcfac176f41821586a0ee
Stephen Pryor <stephenpryor02@yahoo.com>
2013-04-09 21:17:40 -0500

    Added sentiment features

--- scripts/js.js
+++ scripts/js.js
@@ -11,29 +11,52 @@ function removeFade() {
 
 var $iframe;
 
-function playTweets()
-{
+function playTweets(){
   $iframe[0].contentWindow.playTweets();
 }
 
-function resetTweets()
-{
+function resetTweets(){
   $iframe[0].contentWindow.resetTweets();
 }
 
-function displayAllTweets()
-{
+function displayAllTweets(){
   $("#playTweets").removeClass("active").html("Play");
   $iframe[0].contentWindow.displayAllTweets();
 }
 
-function clearTweets()
-{
+function clearTweets(){
   $iframe[0].contentWindow.clearTweets();
   resetTweets();
 }
 
-$(document).ready(function() {
+function hideAll(){
+  clearTweets();
+  $("#resetTweets").removeClass("hide").html("Show All");
+}
+
+function stopPlaying(){ 
+  resetTweets();
+  $("#playTweets").removeClass("active").html("Play");
+}
+
+function changeTypes(){
+  stopPlaying();
+  hideAll();
+}
+
+function useTweets(){
+  changeTypes();
+  $iframe[0].contentWindow.useTweets();
+  clearTweets();
+}
+
+function useSentiment(){ 
+  changeTypes();
+  $iframe[0].contentWindow.useSentiment();
+  clearTweets();
+}
+
+$(document).ready(function(){
   $iframe = $("#usmap");
   $("#usmap").load(function(){
     $(".fadeMessageBox .status").addClass("complete").html("Complete!");
@@ -43,16 +66,18 @@ $(document).ready(function() {
 
   $(".optionList a.option").click(function(){
     if(!$(this).hasClass("selected")){
+      id = $(this).attr("id");
       $(".optionList a.option.selected").removeClass("selected");
       $(this).addClass("selected");
+      if(id == "useTweets") useTweets();
+      else if(id == "useSentiment") useSentiment();
     }
     return false;
   });
 
   $("#playTweets").click(function(){
     if($(this).hasClass("active")){
-      resetTweets();
-      $(this).removeClass("active").html("Play");
+      stopPlaying()
     } else {
       playTweets();
       $(this).addClass("active").html("Stop");
@@ -65,8 +90,7 @@ $(document).ready(function() {
  
   $("#resetTweets").click(function(){
     if($(this).hasClass("hide")) {
-      clearTweets();
-      $(this).removeClass("hide").html("Show All");
+      hideAll();
     } else {
       displayAllTweets();
       $(this).addClass("hide").html("Hide All");
